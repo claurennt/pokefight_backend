7e93e14f31904cc0ce6dea25f767e5d6a2167006
Alin Sergiu Draganescu <alindraganescu@gmail.com>
2021-07-01 17:45:00 +0200

    fetch the pictures

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -19,13 +18,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/pokemon', indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
+
+console.log('test');
 
 // error handler
 app.use((err, req, res, next) => {
@@ -37,5 +37,5 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error');
 });
-
+console.log('test');
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+let jsonData = require('../file.json');
+const axios = require('axios');
 
-/* GET home page. */
+const fetchData = axios
+  .get('https://pokeapi.co/api/v2/pokemon?&limit=1118')
+  .then((data) => {
+    console.log(data.data.results);
+  });
+
+// /* GET home page. */ at /pokemon endpoint
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  // fetch data from external api
+  res.send(jsonData);
 });
 
 module.exports = router;
